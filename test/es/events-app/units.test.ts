52dcb57a161b2644687d21bca6c920b4ec47caa3
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-02 17:38:32 +0100

    RED: Elimina un objeto de un JSON. objeto con correo y contraseña se elimina contraseña y se espera que no devuelva correo y correo

--- test/es/events-app/units.test.ts
+++ test/es/events-app/units.test.ts
@@ -13,6 +13,18 @@ describe('Tests unitarios', () => {
             const deletethis = deleteElement.element(json, 'contraseña');
             expect(deletethis).toStrictEqual({"correo": "email@gmail.com"})
         })
+
+        test('Elimina un objeto de un JSON. objeto con correo y contraseña se elimina contraseña y se espera que no devuelva correo y correo',function(){
+            const json = {
+                "correo": "email@gmail.com",
+                "contraseña": "123456789-"
+            }
+    
+            const deletethis = deleteElement.element(json, 'contraseña');
+            
+            expect(deletethis).toBe({"correo": "email@gmail.com","contraseña": "123456789-"})
+    
+        })
     
     })
 

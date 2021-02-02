aa1a3932332920c11c23861960bdc80bd10e2272
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-02 12:43:17 +0100

    Implementación de método find de la interfaz CRUDI, crea una búsqueda genérica mediante el paso de un objeto ejemplo {email: req.body.email} y devuelve un array.

--- src/es/events-app/Data access/DataCrud.ts
+++ src/es/events-app/Data access/DataCrud.ts
@@ -2,4 +2,9 @@ import { CRUDI } from "./Interfaces/CRUDI";
 
 export class DataCrud implements CRUDI{
 
+    public async find(param:any,req:any,res:any,object:any){
+         return await param.find(object) 
+    }
+
+
 } 

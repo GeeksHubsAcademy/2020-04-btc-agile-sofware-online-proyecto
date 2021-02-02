dcfa2fab6e71a63bb9fa84e005fdd18c1032d358
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-02 14:06:33 +0100

    Creación de método update para actualizar los datos del usuario

--- src/es/events-app/Controller/UserController/UserCrud.ts
+++ src/es/events-app/Controller/UserController/UserCrud.ts
@@ -44,5 +44,26 @@ export class UserCrud extends ControllerCrud {
         }
     }
 
-  
+    async Update(param:any,req:any,res:any,msg:string) {
+        try {
+            const user = await this.CRUD.find(param,req,res,{ email: req.body.email })
+
+            if (!user[0]) {
+                return res.status(404).end('This user does not Exist');
+            }
+
+            const token = await TokenAction.ValToken(user[0]._id, req, res)
+
+            if (token === true) {
+                const userUpdate = await this.CRUD.update(param,req.body,res,{ email: user[0].email })
+                userUpdate.save()
+                return await res.send({ message: msg, object: userUpdate });
+            }
+
+        } catch (error) {
+            return ErrorCatch.errorReturn(error, res, 'There was a problem updating the user')
+        }
+    }
+
+
 }

309c0c05dc8db15ad5195f6a7fcae06612cc9251
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-02 11:02:20 +0100

    Exportación de conexión a servidor mediante express "app" e implementación de framework "Body Parser"

--- src/es/events-app/app.ts
+++ src/es/events-app/app.ts
@@ -1,3 +1,13 @@
 import express = require("express")
 import bodyParser = require("body-parser")
 
+/**************************
+Connection for the server 
+**************************/
+export const app = express();
+
+/**************************
+Body Parser
+**************************/
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())

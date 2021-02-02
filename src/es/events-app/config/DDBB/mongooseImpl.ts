5a66c8aaa2c5492922ce04b5b639dd5bddbc4294
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-02 11:18:31 +0100

    Implementación de clase MongooseCon, importando clase, creando variable para url de base de datos, mensaje de conexión e implementación de la clase MongooseCon pasando variable urlDatabase y Message.

---
+++ src/es/events-app/config/DDBB/mongooseImpl.ts
@@ -0,0 +1,14 @@
+import { MongooseCon } from "./mongoose";
+
+/***********************
+User Database Connection
+************************/
+
+const urlDatabase = 'mongodb://localhost:27017/eventsapp';
+const Message = 'Connected to Database';
+const DatabaseConnection = new MongooseCon(urlDatabase,Message).MongoProcess();
+
+export = urlDatabase;
+ 
+
+

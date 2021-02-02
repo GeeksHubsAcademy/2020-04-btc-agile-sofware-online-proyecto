cd2986fc942cde4399994a25acf1fef78ac81c71
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-02 12:56:28 +0100

    implementación de método Create para la creación de usuario, busca al usuario, si no existe lo crea y lo devuelve en el response si existe devuelve un error 409 informando de que hay conflicto porque ya el usuario existe.

--- src/es/events-app/Controller/UserController/UserCrud.ts
+++ src/es/events-app/Controller/UserController/UserCrud.ts
@@ -1,10 +1,28 @@
 import { DataCrud } from '../../Data access/DataCrud'
 import { ControllerCrud } from '../ControllerCrud'
+import { ErrorCatch } from '../ErrorCatch'
+import { TokenAction } from '../MWS/TokenAction'
 
 export class UserCrud extends ControllerCrud {
 
     constructor() {
         super(new DataCrud())
+    }
+
+    async Create(param:any,req:any,res:any,msg:string) {
+        try {
+            const user = await this.CRUD.find(param,req,res,{ email: req.body.email })
+
+            if (user[0]) {
+                return res.status(409).end('Email already Exist');
+            }
+
+            const createUser = await this.CRUD.create(param,req,res,req.body)
+            return await res.send({ message: msg, object: createUser });
+
+        } catch (error) {
+            return res.send(error,'there was a problem creating the user')
+        }
     }
 
 

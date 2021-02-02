4b343c866ef49c3ba3bec431a45e2981c4ad860d
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-02 14:14:07 +0100

    Test unitario de clase deleteElement, elimina un objeto de un JSON. TO BE (ok)

--- test/es/events-app/units.test.ts
+++ test/es/events-app/units.test.ts
@@ -2,7 +2,7 @@ import { deleteElement } from "../../../src/es/events-app/Controller/MWS/deleteE
 
 describe('Tests unitarios', () => {
 
-    describe('Test unitario de clase deleteElement, elimina un objeto de un JSON. TO BE', () => {
+    describe('Test unitario de clase deleteElement, elimina un objeto de un JSON. TO BE (ok)', () => {
         const json = {
             "correo": "email@gmail.com",
             "contraseña": "123456789-"

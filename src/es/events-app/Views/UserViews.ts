ebb4a0c56fe883160738e5cda4c3cb37e44f0111
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-03 12:06:33 +0100

    Vistas para creación de rutas para endpoints de Usuarios

---
+++ src/es/events-app/Views/UserViews.ts
@@ -0,0 +1,30 @@
+import express = require('express');
+const router =  express.Router();
+import UserControllers = require('../Implements/UserImplement')
+
+/**************************
+Create user
+**************************/
+router.post('/user/register',UserControllers.Create);
+
+/**************************
+Get user
+**************************/
+router.get('/user',UserControllers.Get)
+
+/**************************
+Update user
+ **************************/
+router.put('/user/update',UserControllers.Update);
+
+/**************************
+Delete user
+ **************************/
+router.delete('/user/delete',UserControllers.Delete);
+
+/**************************
+ Logging user
+ **************************/
+router.post('/login',UserControllers.Login)
+
+export = router;

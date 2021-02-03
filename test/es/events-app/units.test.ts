5a308bc2bcee1fb82c665bb984ef096bdb2579fb
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-03 13:00:02 +0100

    GREEN: Test unitario de clase elementsLeft. Resta con dos numeros positivos y restando un numero negativo. Not To be (5+5)-(-2)=12

--- test/es/events-app/units.test.ts
+++ test/es/events-app/units.test.ts
@@ -58,7 +58,7 @@ describe('Tests unitarios', () => {
 
         test('Resta con dos numeros positivos y restando un numero negativo. Not To be (5+5)-(-2)=12', function () {
             let Resta = elementLeft.element((5 + 5), -2)
-            expect(Resta).toBe(8)
+            expect(Resta).not.toBe(8)
         })
 
 

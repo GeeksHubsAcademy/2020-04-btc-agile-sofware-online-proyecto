c18c16b9c6162afd5c86df1377b2df5eb626e67b
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-03 12:47:42 +0100

    GREEN: Test unitario de clase elementsLeft, resta a las invitaciones los invitados que están inscrito en base de datos. test: Resta bien. To be 5-2=3

--- test/es/events-app/units.test.ts
+++ test/es/events-app/units.test.ts
@@ -29,7 +29,7 @@ describe('Tests unitarios', () => {
     
     })
 
-    describe('Test unitario de clase elementsLeft, resta a las invitaciones los invitados que están inscrito en base de datos. ', function () {
+    describe('Test unitario de clase elementsLeft, resta a las invitaciones los invitados que están inscrito en base de datos.', function () {
 
         test('Resta bien. To be 5-2=3', function () {
             let Resta = elementLeft.element(5, 2)

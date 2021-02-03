aa61f2999663c77400a01f9611fb33153c4b9364
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-03 12:45:13 +0100

    RED: Test unitario de clase elementsLeft, resta a las invitaciones los invitados que están inscrito en base de datos. test: Resta bien. To be 5-2=3

--- test/es/events-app/units.test.ts
+++ test/es/events-app/units.test.ts
@@ -1,4 +1,5 @@
 import { deleteElement } from "../../../src/es/events-app/Controller/MWS/deleteElement"
+import { elementLeft } from "../../../src/es/events-app/Controller/MWS/elementsLeft"
 
 describe('Tests unitarios', () => {
 
@@ -28,5 +29,14 @@ describe('Tests unitarios', () => {
     
     })
 
+    describe('Test unitario de clase elementsLeft, resta a las invitaciones los invitados que están inscrito en base de datos. ', function () {
 
+        test('Resta bien. To be 5-2=3', function () {
+            let Resta = elementLeft.element(5, 2)
+            expect(Resta).toBe(3)
+        })
+
+    
+    })
+   
 })

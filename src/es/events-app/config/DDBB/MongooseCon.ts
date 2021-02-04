1505ebf3742b4a700f4774f1493749d445fee480
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-04 14:58:51 +0100

    antiguo fichero llamando "mongoose" cambio de nombre para evitar confusión con el nombre del framework

---
+++ src/es/events-app/config/DDBB/MongooseCon.ts
@@ -0,0 +1,33 @@
+import { mongooseI } from "./mongooseI";
+const mongoose = require('mongoose');
+
+export class MongooseCon implements mongooseI {
+    private _url: String;
+    private _msg: String;
+
+    constructor(url: String, msg: String) {
+        this._url = url;
+        this._msg = msg;
+    }
+
+  async  MongoProcess() {
+
+        const url = this._url;
+        const msg = this._msg
+
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true,
+
+        })
+            .then(async function () {
+                await console.log(msg)
+            })
+            .catch(async function (err){
+                await console.error(err);
+            });
+
+    }
+}

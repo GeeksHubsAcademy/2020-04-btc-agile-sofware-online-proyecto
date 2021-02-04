3769b3e8b0b1c3d30ad2beeb8af8b744277a042f
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-04 12:06:04 +0100

    Creación de test de integración para los endpoints usando Jest y supertest.

---
+++ test/es/events-app/endpoints.test.ts
@@ -0,0 +1,10 @@
+const request = require("supertest");
+import { app } from '../../../src/es/events-app/app';
+const mongoose = require('mongoose');
+
+
+describe('Test de endpoints, respuestas de conexión', () => {
+
+
+
+})

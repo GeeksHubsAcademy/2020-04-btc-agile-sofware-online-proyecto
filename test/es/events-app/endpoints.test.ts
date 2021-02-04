f47ab00959ad2379fb82380cefe7ed356a8ae6ae
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-04 12:08:04 +0100

    RED: Registro de usuario password menos de 8 carácteres, expected (500)

--- test/es/events-app/endpoints.test.ts
+++ test/es/events-app/endpoints.test.ts
@@ -4,6 +4,35 @@ const mongoose = require('mongoose');
 
 
 describe('Test de endpoints, respuestas de conexión', () => {
+    
+    const email1 = "email@gmail.com"
+    const email2 = "email2@gmail.com"
+    let token;
+
+    describe('Test de endpoints usuario', () => {
+
+        it('Registro de usuario password menos de 8 carácteres, expected (500)', (done) => {
+            request(app)
+                .post('/user/register')
+                .send({
+                    "username": "username",
+                    "email": email1,
+                    "password": "1234567-",
+                    "name": "Manuel",
+                    "lastname": "Guerra Coello"
+                })
+                .set('Accept', 'application/json')
+                .expect(res => {
+                    expect(res.status).toBe(500)
+                    expect(res.body.error).toBe('Password is shorter than the minimum allowed length (8)')
+                })
+                .end(async (err) => {
+                    if (err) return await done(err);
+                    done();
+                })
+
+        })
+    })
 
 
 

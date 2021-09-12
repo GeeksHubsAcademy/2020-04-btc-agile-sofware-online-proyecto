f9c14773647642e01f8d0cc6055d4590894fb7f6
biorubenfs <biorubenfs@hotmail.com>
2021-09-12 13:01:36 +0200

    New front tests added with cypress

--- front/cypress/integration/books/books.spec.js
+++ front/cypress/integration/books/books.spec.js
@@ -1,24 +1,77 @@
-describe("Testing New Book Form", () => {
+describe('Testing Books', () => {
 
     let seedBooks = 4;
+    const url = 'http://localhost:3000/';
 
-    context("Add book", () => {
+    context('Add new Book', () => {
 
-        it("Checking book list", () => {
+        it('Checking book list', () => {
             cy.viewport(1400, 800);
-            cy.visit("http://localhost:3000/");
-            cy.get(".display-books-box div").should("have.length", seedBooks);
-        })
+            cy.visit(url);
+            cy.get('.display-books-box div').should('have.length', seedBooks);
+        });
 
-        it("Add book", () => {
+        it('Add book', () => {
             cy.viewport(1400, 800);
-            cy.visit("http://localhost:3000/");
-            cy.get("#title").type("random title");
-            cy.get("#author").type("random author");
-            cy.get("#year").type("2000");
-            cy.get(".add-button").click();
-            cy.get(".display-books-box div").should("have.length", ++seedBooks);
-        })
-    })
+            cy.visit(url);
 
+            cy.get('.form').within(() => {
+                cy.get('#title')
+                    .type('Notas al pie de Gaza')
+                    .invoke('prop', 'validity')
+                    .should('deep.include', {
+                        valid: true
+                    });
+                cy.get('#author')
+                    .type('Joe Sacco')
+                    .invoke('prop', 'validity')
+                    .should('deep.include', {
+                        valid: true
+                    });
+                cy.get('#year')
+                    .type('2009')
+                    .invoke('prop', 'validity')
+                    .should('deep.include', {
+                        valid: true
+                    });
+                cy.get('.add-button').click();
+            });
+
+            cy.get('.display-books-box div').should('have.length', ++seedBooks);
+            cy.get('.display-books-box').contains(':nth-child(5) > h2', 'Notas al pie de Gaza');
+        });
+
+        it('Form fields validation validation', () => {
+            cy.viewport(1400, 800);
+            cy.visit(url);
+
+            cy.get('.form').within(() => {
+                cy.get('#title')
+                    .invoke('prop', 'validity')
+                    .should('deep.include', {
+                        valid: false
+                    });
+                cy.get('#author')
+                    .invoke('prop', 'validity')
+                    .should('deep.include', {
+                        valid: false
+                    });
+                cy.get('#year')
+                    .invoke('prop', 'validity')
+                    .should('deep.include', {
+                        valid: false
+                    });
+            });
+        });
+    });
+
+    context('Delete Book', () => {
+
+        it('Delete book', () => {
+            cy.viewport(1400, 800);
+            cy.visit(url);
+            cy.get(':nth-child(3) > .delete-button').click();
+            cy.get('.display-books-box div').should('have.length', --seedBooks);
+        });
+    });
 })
